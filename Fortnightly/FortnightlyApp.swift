1bba273dc8eb0eeea86a216fcbb1e084e64289eb
Vago Benedek <vagobenedek@Vago-MacBook-Pro.local>
2025-03-21 08:50:41 +0100

    Initial commit

---
+++ Fortnightly/FortnightlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FortnightlyApp.swift
+//  Fortnightly
+//
+//  Created by Vago Benedek on 2025. 03. 21..
+//
+
+import SwiftUI
+
+@main
+struct FortnightlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
